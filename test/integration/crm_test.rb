09b4215bd1318356c73fd1898c840003ad43db83
Ryan Mindigo <ryanmindigo@Ryans-MacBook-Air.local>
2017-03-14 18:26:16 -0700

    more changes

---
+++ test/integration/crm_test.rb
@@ -0,0 +1,74 @@
+require "selenium-webdriver"
+require "rspec"
+
+
+describe "Get the title" do 
+
+	before(:each) do
+	    # Input capabilities
+		caps = Selenium::WebDriver::Remote::Capabilities.new
+		caps["browser"] = "IE"
+		caps["browser_version"] = "7.0"
+		caps["os"] = "Windows"
+		caps["os_version"] = "XP"
+		caps["browserstack.debug"] = "true"
+		caps["browserstack.local"] = "true"
+		caps["name"] = "Testing MyCrm"
+
+		@driver = Selenium::WebDriver.for(:remote,
+		  :url => "http://ryanmindigo1:yiDYpaqBwo6Ae47ZxkVn@hub-cloud.browserstack.com/wd/hub",
+		  :desired_capabilities => caps)
+	    @base_url = "http://localhost:3000/"
+	    @accept_next_alert = true
+	    @driver.manage.timeouts.implicit_wait = 30
+	 end
+	 
+	 after(:each) do
+	    @driver.quit   
+	 end
+
+	it 'check title' do 
+		@driver.navigate.to @base_url
+		title = @driver.title
+		expect(title).to match "MyCrm"
+	end
+
+	it 'sign up' do
+		@driver.navigate.to @base_url
+
+		click_here = @driver.find_element(:id, "click_here")
+
+		click_here.click
+
+		sleep(3)
+
+		source = @driver.page_source
+
+		expect(source).to include('Sign up') 
+
+		email = @driver.find_element(:id, 'user_email')
+
+		email.send_keys 'ryanm@pick.co'
+
+		pswd = @driver.find_element(:id, 'user_password')
+
+		pswd.send_keys 'changeme'
+
+		pswdConfirm = @driver.find_element(:id, 'user_password_confirmation')
+
+		pswdConfirm.send_keys 'changeme'
+
+		submit = @driver.find_element(:name, 'commit')
+
+		submit.click
+
+		source = @driver.page_source
+
+		expect(source).to include('Contacts') 
+
+		
+	end
+end
+
+
+
