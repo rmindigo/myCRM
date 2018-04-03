321475d79a61cc1c87c1eff4bcfbdb47756dee93
Ryan Mindigo <ryanmindigo@Ryans-MacBook-Air.local>
2018-04-03 09:50:28 -0700

    small change

--- test/integration/crm_test.rb
+++ test/integration/crm_test.rb
@@ -56,7 +56,7 @@ describe "Get the title" do
 
 		email = @driver.find_element(:id, 'user_email')
 
-		email.send_keys 'ryanmmm91233@pick.co'
+		email.send_keys 'ryanmmm9984@pick.co'
 
 		pswd = @driver.find_element(:id, 'user_password')
 
